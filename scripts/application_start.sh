7c32645330f1ddc091b4de5a4aca0bfb4f078845
thiagotar <thiago-tar@hotmail.com>
2023-01-09 05:19:55 +1030

    test deploy

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -11,6 +11,7 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # loads nvm	
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # loads nvm bash_completion (node is in path now)
 
+cd /home/ec2-user/deploy/api/email-api
 #install node modules
 npm install
 
