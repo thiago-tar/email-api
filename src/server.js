a4e6704f222590d62bbb137b2ad339c96b03cba3
thiagotar <thiago-tar@hotmail.com>
2023-01-09 03:25:42 +1030

    changed port server

--- src/server.js
+++ src/server.js
@@ -1,3 +1,3 @@
 const app = require('./app');
-const port = process.env.EMAIL_API_PORT || 2000;
+const port = process.env.EMAIL_API_PORT || 3000;
 app.listen(port, () => console.log('Listen on port: ' + port));
