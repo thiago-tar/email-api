fb58f5d468a83d8da8b2bd5e354de6cc11a8995c
thiagotar <thiago-tar@hotmail.com>
2023-01-09 03:17:02 +1030

    fixed wrong command

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 #give permission for everything in the express-app directory
-sudo chmod -R 777 /home/ec2-user/express-app
+sudo chmod -R 777 /home/ec2-user/deploy/api/email-api
 
 #navigate into our working directory where we have all our github files
-cd /home/ec2-user/apis/git-deploy/email-api
+cd /home/ec2-user/deploy/api/email-api
 
 #add npm and node to path
 export NVM_DIR="$HOME/.nvm"	
--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -3,10 +3,10 @@ curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
 nvm install node
 
 #create our working directory if it doesnt exist
-DIR="/home/ec2-user/apis/git-deploy/email-api"
+DIR="/home/ec2-user/deploy/api/email-api"
 if [ -d "$DIR" ]; then
   echo "${DIR} exists"
 else
   echo "Creating ${DIR} directory"
-  mkdir ${DIR}
+  mkdir -p ${DIR}
 fi
