66e01c22f559430670e4391c3008cbbe49912133
thiagotar <thiago-tar@hotmail.com>
2023-01-09 05:59:17 +1030

    changed nvm version

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,4 +1,4 @@
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
 . ~/.nvm/nvm.sh
 nvm install 16.17
 
