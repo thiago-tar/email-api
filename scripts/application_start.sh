300ee2bad6573cc4c0022f6815517562a403f97c
thiagotar <thiago-tar@hotmail.com>
2023-01-09 14:02:22 +1030

    changes

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -1,17 +1,11 @@
 #!/bin/bash
 
-#give permission for everything in the express-app directory
+#give permission for everything in directory
 sudo chmod -R 777 /home/ec2-user/deploy/api/email-api
 
 #navigate into our working directory where we have all our github files
 cd /home/ec2-user/deploy/api/email-api
 
-#add npm and node to path
-export NVM_DIR="$HOME/.nvm"	
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # loads nvm	
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # loads nvm bash_completion (node is in path now)
-
-cd /home/ec2-user/deploy/api/email-api
 echo "NPM INSTALL"
 #install node modules
 npm install
--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,7 +1,3 @@
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
-. ~/.nvm/nvm.sh
-nvm install 16.17
-
 #create our working directory if it doesnt exist
 DIR="/home/ec2-user/deploy/api/email-api"
 if [ -d "$DIR" ]; then
