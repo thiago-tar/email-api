c49e16251acf39daaf622b2cae22a06a7725cd97
thiagotar <thiago-tar@hotmail.com>
2023-01-09 15:58:04 +1030

    teste

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
 
-#give permission for everything in directory
+#give permission for everything in the express-app directory
 sudo chmod -R 777 /home/ec2-user/deploy/api/email-api
 
 #navigate into our working directory where we have all our github files
 cd /home/ec2-user/deploy/api/email-api
+
+#add npm and node to path
+# export NVM_DIR="$HOME/.nvm"	
+# [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # loads nvm	
+# [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # loads nvm bash_completion (node is in path now)
 
 echo "NPM INSTALL"
 #install node modules
 npm install
 echo "NPM START"
 #start our node app in the background
-npm start
+node ./src/server.js > app.out.log 2> app.err.log < /dev/null & 
 echo "Finish"
