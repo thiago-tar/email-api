00b1de59df1d6c77d7219de06a5116cedcaae3bc
thiagotar <thiago-tar@hotmail.com>
2023-07-15 18:34:58 +0930

    testing deploy

--- scripts/application_stop.sh
+++ scripts/application_stop.sh
@@ -1,8 +1,8 @@
-echo "stopping any existing node servers"
-STATUS="$(systemctl is-active EmailApi.service)"
-if [ "${STATUS}" = "active" ]; then
-    sudo systemctl stop EmailApi.service
-    sudo systemctl disable EmailApi.service
-    sudo rm -f /etc/systemd/system/EmailApi.service 
-    echo "Execute your tasks ....."
-fi
+# echo "stopping any existing node servers"
+# STATUS="$(systemctl is-active EmailApi.service)"
+# if [ "${STATUS}" = "active" ]; then
+#     sudo systemctl stop EmailApi.service
+#     sudo systemctl disable EmailApi.service
+#     sudo rm -f /etc/systemd/system/EmailApi.service 
+#     echo "Execute your tasks ....."
+# fi
