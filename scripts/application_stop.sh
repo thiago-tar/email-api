9606a988a1a5e3e3e079a928f7891cce44cd702a
thiagotar <thiago-tar@hotmail.com>
2023-07-15 18:31:34 +0930

    testing deploy

--- scripts/application_stop.sh
+++ scripts/application_stop.sh
@@ -3,5 +3,6 @@ STATUS="$(systemctl is-active EmailApi.service)"
 if [ "${STATUS}" = "active" ]; then
     sudo systemctl stop EmailApi.service
     sudo systemctl disable EmailApi.service
-    sudo rm -f /etc/systemd/system/EmailApi.serviceecho "Execute your tasks ....."
+    sudo rm -f /etc/systemd/system/EmailApi.service 
+    echo "Execute your tasks ....."
 fi
