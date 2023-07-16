1f6c150b3868a267daa176c709824edca571e206
thiagotar <thiago-tar@hotmail.com>
2023-07-16 20:02:48 +0930

    testing cors

--- src/app.js
+++ src/app.js
@@ -7,7 +7,7 @@ const routes = require("./routes");
 const app = express();
 
 app.get("/", (req, res) => res.send("Is Listenning."));
-app.use(cors(corsOptions));
+app.use(cors());
 app.use(allowedAPIS.checkAllowedApi);
 app.use(express.json());
 app.use(routes);
